4d510a8d2d7c3bdd01a7e22978e50ae2d530185d
MrMernDeveloper <rakibkst16@gmail.com>
2022-10-25 16:57:38 +0600

    added title and favicon

---
+++ src/UserContext/UserContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserContext = ({children}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserContext;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCW7NPDMLIRs_YNyfUmoItfMgXANzRUct8",
+    authDomain: "online-learning-site.firebaseapp.com",
+    projectId: "online-learning-site",
+    storageBucket: "online-learning-site.appspot.com",
+    messagingSenderId: "897938315825",
+    appId: "1:897938315825:web:eabc8eaaf8fa897307d310"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
